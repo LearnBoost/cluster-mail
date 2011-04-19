65935fc9a868407e1aca05193ea6cfae5e19966d
Tj Holowaychuk <tj@vision-media.ca>
2011-04-19 14:28:27 -0700

    added arbitrary details support

--- example.js
+++ example.js
@@ -8,20 +8,21 @@ var http = require('http')
   , mail = require('./');
 
 var server = http.createServer(function(req, res){
-  if (Math.random() > 0.9) throw new Error('fail!');
-  res.end('Hello World');
+  // uncaught exceptions are automatically reported, however
+  // for exceptions that we have caught, typically within
+  // an error handler, we may report them with cluster.reportException()
+  try {
+    if (Math.random() > 0.9) throw new Error('fail!');
+    res.end('Hello World');
+  } catch (err) {
+    // we can also optionally send an arbitrary json object
+    // for use within our template
+    var data = { method: req.method, url: req.url };
+    cluster.mailException(err, data);
+    console.log('sent %s', err.message);
+  }
 });
 
 cluster = cluster(server)
   .use(mail('tjholowayhuk@gmail.com'))
   .listen(3000);
-
-if (cluster.isWorker) {
-  setTimeout(function(){
-    // we can use cluster.call() from within a worker
-    // to notify master of an exception that was not uncaught
-    console.log('timed out');
-    var err = new Error('timeout!');
-    cluster.mailException(err);
-  }, 5000);
-}
